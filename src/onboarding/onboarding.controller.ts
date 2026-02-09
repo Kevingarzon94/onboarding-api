ca096d00d7ae9ebb1d8e01e3186fb157cd1e8024
Kevin Garzon <kevingarzon4@gmail.com>
2026-02-09 02:15:28 -0500

    feature/Add controller to onboarding

--- src/onboarding/onboarding.controller.ts
+++ src/onboarding/onboarding.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Get } from '@nestjs/common';
 import { OnboardingService } from './onboarding.service';
+import { CreateOnboardingDto } from './dto/create-onboarding.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('onboarding')
 export class OnboardingController {
-  constructor(private readonly onboardingService: OnboardingService) {}
+  constructor(private readonly onboardingService: OnboardingService) { }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  create(@Body() createOnboardingDto: CreateOnboardingDto) {
+    return this.onboardingService.create(createOnboardingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.onboardingService.findAll();
+  }
 }
